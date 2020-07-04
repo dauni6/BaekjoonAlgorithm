e31d2573feb791fcf3a455ed05a9b1f178f21908
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-07-04 21:51:32 +0900

    update

---
+++ src/bronze5/Problem10926.java
@@ -0,0 +1,13 @@
+package bronze5;
+
+import java.io.*;
+
+public class Problem10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String word = br.readLine();
+        bw.write(word + "??!");
+        bw.flush();
+    }
+}
