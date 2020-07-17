29322ef242765326e873eea52e25de5a5e471ede
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-07-17 22:50:07 +0900

    update
    update

--- src/bronze5/Problem11283.java
+++ src/bronze5/Problem11283.java
@@ -4,7 +4,11 @@ import java.io.*;
 
 /*유니코드 한글은 0xAC00 으로부터
         초성 19개, 중상21개, 종성28개로 이루어지고
-        이들을 조합한 11,172개의 문자를 갖는다.*/
+        이들을 조합한 11,172개의 문자를 갖는다.
+
+        한글 시작 코드 : 44032 (0xAC00)
+        한글 끝 코드   : 55199 (0xD79F)
+        */
 public class Problem11283 {
 
     public static void main(String[] args) throws IOException {
