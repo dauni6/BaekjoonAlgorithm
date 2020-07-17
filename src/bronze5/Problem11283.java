f8bb0605712033bbd929ed37766fd4eb893076ea
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-07-17 23:03:26 +0900

    update
    update

--- src/bronze5/Problem11283.java
+++ src/bronze5/Problem11283.java
@@ -2,9 +2,11 @@ package bronze5;
 
 import java.io.*;
 
-/*유니코드 한글은 0xAC00 으로부터
+        /*유니코드 한글은 0xAC00 으로부터
         초성 19개, 중상21개, 종성28개로 이루어지고
         이들을 조합한 11,172개의 문자를 갖는다.
+
+        한글코드의 값 = ((초성 * 21) + 중성) * 28 + 종성 + 0xAC00
 
         한글 시작 코드 : 44032 (0xAC00)
         한글 끝 코드   : 55199 (0xD79F)
@@ -16,7 +18,7 @@ public class Problem11283 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String word = br.readLine(); //글자입력
-
+        int that = word.charAt(0);
         int result = word.charAt(0)-0xAC00 + 1;
 
         bw.write(result + "");
