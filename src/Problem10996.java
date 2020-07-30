881325bf87b45ca5c7ec1dfa4f5a0b893183a356
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-07-30 18:48:00 +0900

    update
    update

--- src/Problem10996.java
+++ src/Problem10996.java
@@ -1,5 +1,25 @@
+import java.io.*;
+
 public class Problem10996 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.valueOf(br.readLine()); // n == 2 라면?
+
+        for (int i = 0; i < n; i++) { //홀수만
+            if (i % 2 != 0) { //홀수
+                System.out.print("*");
+            }
+            System.out.print(" ");
+        }
+
+        for (int i = 0; i < n; i++) { //짝수만
+            if (i % 2 == 0) {
+                System.out.print(" *");
+            }
+        }
+
     }
 }
