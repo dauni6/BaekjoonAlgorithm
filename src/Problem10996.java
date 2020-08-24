12501d85320cecfd5818d850b8c5e91d5cf030b0
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-08-24 22:18:31 +0900

    update
    update

--- src/Problem10996.java
+++ src/Problem10996.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.logging.Logger;
 
 public class Problem10996 {
 
@@ -9,6 +10,8 @@ public class Problem10996 {
 
         int n = Integer.valueOf(br.readLine()); // n == 2 라면?
         for (int k = 0; k < n; k++) { //전체 반복
+            //1부터 시작해야되는 이유는 i를 0부터 하게되면 시작부터 짝수로 시작하게 된다.
+            //문제도 별 하나부터 시작하기 때문에 출발을 i를 1부터 해줘야 홀수부터 시작 할 수 있다.
             for (int i = 1; i <= n; i++) {
                 if (i%2 != 0) { //홀수
                     bw.write("* ");
@@ -18,12 +21,14 @@ public class Problem10996 {
             bw.write("\n");
 
             for (int i = 1; i <= n; i++) {
-                if (i%2 == 0) {
+                if (i%2 == 0) { //짝수
                     bw.write(" *");
                 }
             }
             bw.write("\n");
         }
         bw.flush();
+        bw.close();
+        br.close();
     }
 }
